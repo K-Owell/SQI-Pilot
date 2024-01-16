b9dc7ce502874ecbe8865167f425f86ef53749a6
Kyle McDowell <k.mcdowellr@gmail.com>
2024-01-15 21:36:47 -0500

    changed enemy collision mask and layer to 3, changed Box collision layer to 1 and 3. enemies no longer collide with player.

--- Mobs/enemy.gd
+++ Mobs/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 150.0
-	move_and_slide()
+	move_and_collide(velocity * delta)
